d80ee6a677dc0b41385c2795355e1e6a5d2a7806
siyabonga <siyabonga.auo@gmail.com>
2022-06-13 20:32:08 +0200

    all CRUD operations are working

--- AvatarAssignment 2MVC/Controllers/FightersController.cs
+++ AvatarAssignment 2MVC/Controllers/FightersController.cs
@@ -62,5 +62,33 @@ namespace AvatarAssignment_2MVC.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+        //Delete 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Fighters.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //Post Delete 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Fighters.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Fighters.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
